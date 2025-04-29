03460668b30cb48f0a9cfc5a99ed3936bbbe27ec
Prabhudayal636 <prabhudayaljanapati@gmail.com>
2025-04-29 21:58:19 +0530

    Fix port issue and add root route

--- server.js
+++ server.js
@@ -4,10 +4,19 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.send('Cashfree Webhook Server is running 🚀');
+  });
+  
+
 app.post("/webhook", (req, res) => {
   console.log("Cashfree Webhook Received:", req.body);
   res.status(200).send("Webhook received");
 });
+app.get('/webhook', (req, res) => {
+    res.send('POST endpoint only. Try sending a POST request.');
+  });
+  
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
